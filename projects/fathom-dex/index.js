7a9246830c66261c01db05ce1afde4758f524961
codinghistorian <gaudihaus@gmail.com>
2024-04-03 13:43:23 +0400

    Fathom-protocol TVL combine 2
    - adding FathomVault

--- projects/fathom-dex/index.js
+++ projects/fathom-dex/index.js
@@ -2,15 +2,21 @@ const sdk = require("@defillama/sdk");
 const { getUniTVL } = require("../helper/unknownTokens");
 
 const WXDC = '0x951857744785e80e2de051c32ee7b25f9c458c42';
-const vaultAddress = '0x9B4aCeFE2dB986Ca080Dc01d137e6566dBE0aA3a';
+const WXDCHolders = '0x9B4aCeFE2dB986Ca080Dc01d137e6566dBE0aA3a'; // CDP Vault CollateralPoolId "0x5844430000000000000000000000000000000000000000000000000000000000"
 
-const fetchWXDCBalanceInVault = async (timestamp, block, chainBlocks) => {
+const FXD = '0x49d3f7543335cf38Fa10889CCFF10207e22110B5';
+const FXDHolders = [
+  "0x3C8e9896933B374E638f9a5C309535409129aaA2", // Vault0
+  "0xE2DEa7e0c272dE04e8708674dAE73ebd6E5c1455", // Strategy0 of Vault0
+]
+
+const fetchWXDCBalances = async (timestamp, block, chainBlocks) => {
   const balances = {};
   const chain = 'xdc';
   const blockXdc = chainBlocks[chain];
   const balance = await sdk.api.erc20.balanceOf({
     target: WXDC,
-    owner: vaultAddress,
+    owner: WXDCHolders,
     block: blockXdc,
     chain,
   });
@@ -19,6 +25,23 @@ const fetchWXDCBalanceInVault = async (timestamp, block, chainBlocks) => {
   return balances;
 };
 
+const fetchFXDBalances = async (timestamp, block, chainBlocks) => {
+  const balances = {};
+  const chain = 'xdc';
+  const blockXdc = chainBlocks[chain];
+  for (const fxdHolder of FXDHolders) {
+    const balance = await sdk.api.erc20.balanceOf({
+      target: FXD,
+      owner: fxdHolder,
+      block: blockXdc,
+      chain,
+    });
+
+    sdk.util.sumSingleBalance(balances, `${chain}:${FXD}`, balance.output);
+  }
+  return balances;
+}
+
 module.exports = {
   xdc: {
     tvl: sdk.util.sumChainTvls([
@@ -26,7 +49,8 @@ module.exports = {
         factory: '0x9fAb572F75008A42c6aF80b36Ab20C76a38ABc4B',
         useDefaultCoreAssets: true,
       }),
-      fetchWXDCBalanceInVault,
+      fetchWXDCBalances,
+      fetchFXDBalances,
     ]),
   },
 };
